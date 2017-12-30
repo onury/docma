8f94b25e5e5d75e8406d5abbaf7aa91d758130d6
Onur Yıldırım <onur@cutepilot.com>
2017-12-30 03:23:48 +0300

    add support for docma.config.json destination

--- bin/serve.js
+++ bin/serve.js
@@ -9,6 +9,8 @@ const fs = require('fs-extra');
 const serveStatic = require('serve-static');
 const finalhandler = require('finalhandler');
 const chalk = require('chalk');
+// own modules
+const utils = require('../lib/utils');
 
 const indexFiles = [];
 ['index', 'default', 'main', 'home', 'readme', 'guide']
@@ -23,13 +25,25 @@ module.exports = (spaPath, options) => {
 
     const url = `http://localhost:${options.port}`;
     const docmaPath = path.join(__dirname, '..');
-    const servePath = spaPath
+    const pathIsSet = Boolean(spaPath);
+    let servePath = pathIsSet
         ? path.resolve(docmaPath, spaPath)
         : process.cwd();
 
     if (!fs.pathExistsSync(servePath)) {
         console.error(chalk.red(`Cannot serve "${spaPath}". Path does not exist!`));
         return;
+    }
+
+    const docmaConfigPath = path.join(servePath, 'docma.config.json');
+    if (!pathIsSet && fs.pathExistsSync(docmaConfigPath)) {
+        console.info(chalk.gray(`» Found docma.config.json @ ${servePath}`));
+        console.info(chalk.gray(`  I will attempt to serve the configured destination.`));
+        console.info(chalk.gray(`  Provide a path (e.g. "./") to force serve target directory...`));
+        const conf = utils.json.readSync(docmaConfigPath);
+        if (conf && typeof conf.dest === 'string') {
+            servePath = path.resolve(docmaPath, conf.dest);
+        }
     }
 
     console.info(chalk.cyan(`Starting server @ path: ${servePath}`));
