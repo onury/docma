fd8d6d2cf1fe6050bf704b9674466e46961fae27
Onur Yıldırım <onur@cutepilot.com>
2018-02-10 06:25:09 +0300

    clean up

--- templates/zebra/src/js/app.filters.js
+++ templates/zebra/src/js/app.filters.js
@@ -1,4 +1,4 @@
-/* global app, docma, dust, hljs, $ */
+/* global app, docma */
 /* eslint camelcase:0, no-nested-ternary:0, max-depth:0, no-var:0, prefer-template:0, prefer-arrow-callback:0, prefer-spread:0, object-shorthand:0 */
 
 (function () {
