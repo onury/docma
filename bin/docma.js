9bd53bcbc54af185d5a8d69668492abc92b6aa90
Onur Yıldırım <onur@cutepilot.com>
2018-04-07 20:29:46 +0300

    add template commands

--- bin/docma.js
+++ bin/docma.js
@@ -21,6 +21,7 @@ const chalk = require('chalk');
 const Docma = require('../lib/docma');
 const serve = require('./commands/serve');
 const doctor = require('./commands/template.doctor');
+const init = require('./commands/template.init');
 const pkg = require('../package.json');
 const utils = require('../lib/utils');
 
@@ -28,8 +29,8 @@ const utils = require('../lib/utils');
 // CLI SETUP
 // --------------------------
 
-const examples = 'Examples:\n\n'
-    + '  * Build documentation with a Docma configuration (JSON) file:\n'
+const examples = 'Examples:\n'
+    + '\n  * Build documentation with a Docma configuration (JSON) file:\n'
     + chalk.white('      docma -c path/to/docma.json')
     + '\n  * If a docma.json exists in the current directory, simply:\n'
     + chalk.white('      docma')
@@ -39,10 +40,29 @@ const examples = 'Examples:\n\n'
     + chalk.white('      docma -c path/to/docma.json -s path/to/lib-1.js -s path/to/lib-2.js')
     + '\n  * Define name-grouped source files:\n'
     + chalk.white('      docma -c path/to/docma.json -s mylib:path/to/lib-1.js -s mylib:path/to/lib-2.js')
-    + '\n  * Serve generated SPA at the default port (9000):\n'
-    + chalk.white('      docma serve /path/to/docs')
+    + '\n'
+    + '\n  * See help for `serve` command:\n'
+    + chalk.white('      docma serve --help')
+    + '\n  * See help for `template` command:\n'
+    + chalk.white('      docma template --help');
+
+const serveExamples = 'Examples:\n'
+    + '\n  * Serve generated SPA at the default port(9000):\n'
+    + chalk.white('      docma serve path/to/docs')
+    + '\n  * Serve current working directory:\n'
+    + chalk.white('      cd path/to/docs && docma serve')
+    + '\n  * Serve configured destination directory (looks for a docma.json):\n'
+    + chalk.white('      cd path/to/project && docma serve')
     + '\n  * Configure a custom port and serve:\n'
-    + chalk.white('      docma serve /path/to/docs -p 8080');
+    + chalk.white('      docma serve path/to/docs -p 8080')
+    + '\n  * Serve without request logs\n'
+    + chalk.white('      docma serve path/to/docs -q');
+
+const templateDoctorExamples = 'Examples:\n'
+    + '\n  * Diagnose a Docma template module:\n'
+    + chalk.white('      docma template doctor path/to/project')
+    + '\n  * Diagnose a template in current working directory:\n'
+    + chalk.white('      cd path/to/project && docma template doctor');
 
 const info = '\n\n'
     + chalk.yellow('Docma Repo ') + '@ ' + chalk.blue('https://github.com/onury/docma')
@@ -54,74 +74,102 @@ const info = '\n\n'
 console.log();
 
 const argv = yargs
-    .usage('Usage: docma [options]') // 'Usage: $0 <cmd> [options]'
+    .usage('docma [command] [options]\n\nBuild documentation from Javascript, Markdown and HTML files.') // 'Usage: $0 <cmd> [options]'
     .help('h', 'Show this help').alias('h', 'help')
     .version('v', 'Output Docma version', pkg.version).alias('v', 'version')
     .option('c', {
         alias: 'config',
         type: 'string',
         description: 'Docma JSON configuration file path. You can define all build options within this file. Any option (below) set via CLI will overwrite the value defined in this file.',
+        global: false,
         normalize: true // apply path.normalize
     })
     .option('s', {
         alias: 'src',
         type: 'array',
         description: '<[name:]path>   Source file path. To define multiple source files, this option can be used more than once. To group/name source files, prefix each path with "name:" e.g. -s a.js -s mylib:b.js -s mylib:c.js -s other:d.js',
+        global: false,
         normalize: true
     })
     .option('d', {
         alias: 'dest',
         type: 'string',
-        description: '<path>   Destination output directory path. CAUTION: This directory will be emptied.',
+        description: '<path>   Destination output directory path.',
+        global: false,
         normalize: true
+    })
+    .option('clean', {
+        type: 'boolean',
+        description: 'Whether to empty destination directory before building.',
+        global: false
     })
     .option('web-logs', {
         type: 'boolean',
-        description: '(Debug) Enable logs in the browser console, for the generated SPA.'
+        description: '(Debug) Enable logs in the browser console, for the generated SPA.',
+        global: false
     })
     .option('V', {
         alias: 'verbose',
         type: 'boolean',
-        description: '(Debug) Output verbose logs to consoles.'
+        description: '(Debug) Output verbose logs to consoles.',
+        global: false
     })
     .option('nomin', {
         type: 'boolean',
-        description: '(Debug) Disable minification for the generated SPA assets. (e.g. js, css files)'
+        description: '(Debug) Disable minification for the generated SPA assets. (e.g. js, css files)',
+        global: false
     })
     .option('jd-out', {
         type: 'boolean',
-        description: '(Debug) Output one or more [name.]jsdoc.json files for each (name-grouped) javascript source.'
+        description: '(Debug) Output one or more [name.]jsdoc.json files for each (name-grouped) javascript source.',
+        global: false
     })
     .option('debug', {
         type: 'boolean',
-        description: 'Enable all debugging options. Equivalent to: --web-logs -v --nomin --jd-out'
+        description: 'Enable all debugging options. Equivalent to: --web-logs -v --nomin --jd-out',
+        global: false
     })
     .option('q', {
         alias: 'quite',
         type: 'boolean',
-        description: '(Debug) Disable build logs for the Node console.'
+        description: '(Debug) Disable build logs for the Node console.',
+        global: false
     })
-    .command('serve [spaPath]', 'Start a static server for the generated SPA.')
-    .option('p', {
-        alias: 'port',
-        describe: 'Port number to bind the mock-server on.',
-        type: 'number',
-        default: 9000
-    })
-    .command('template init [path]', 'Initialize a new Docma template.', yargs => {
-        // yargs
-        //     .options({
-        //         v: { alias: 'verbose', default: true }
-        //     });
-    })
-    .command('template doctor [path]', 'Diagnose a Docma template.', yargs => {
+    .command('serve [path]', 'Start a static server for the generated SPA.', yargs => {
         yargs
             .options({
-                // v: { alias: 'verbose', default: true },
-                first: {
-                    describe: 'Whether to stop on first failure when diagnosing a template.',
-                    default: false
+                p: {
+                    alias: 'port',
+                    describe: 'Port number to bind the mock-server on.',
+                    type: 'number',
+                    default: 9000,
+                    global: false
+                },
+                q: {
+                    alias: 'quite',
+                    type: 'boolean',
+                    description: 'Disable request logs for the Node console.',
+                    global: false
                 }
+            })
+            .epilog(serveExamples + info);
+    })
+    .command('template <cmd>', 'Execute template related commands.', yargs => {
+        yargs
+            .command('init [path]', 'Initialize a new Docma template module.', yargs => {
+                // yargs.options();
+            })
+            .command('doctor [path]', 'Diagnose a Docma template.', yargs => {
+                yargs
+                    .options({
+                        // v: { alias: 'verbose', default: true },
+                        first: {
+                            describe: 'Whether to stop on first fault when diagnosing the template.',
+                            default: false,
+                            global: false
+                        }
+                    })
+                    .epilog(templateDoctorExamples + info);
             });
     })
     .wrap(80)
@@ -154,6 +202,7 @@ function updateConfig(config) {
     }
 
     if (argv.dest) config.dest = argv.dest;
+    if (argv.clean) config.clean = Boolean(argv.clean);
 
     // DEBUG OPTIONS
 
@@ -221,13 +270,14 @@ function getConfigFileSync() {
 const cmds = argv._ || [];
 // checking for `serve` command
 if (cmds.indexOf('serve') >= 0) {
-    serve(argv.spaPath, {
+    serve(argv.path, {
         port: argv.port,
         quite: argv.quite
     });
 } else if (cmds.indexOf('template') >= 0) {
-    if (argv.doctor) {
-        console.log(argv);
+    if (cmds.indexOf('init') >= 0) {
+        init(argv.path);
+    } else if (cmds.indexOf('doctor') >= 0) {
         doctor(argv.path, {
             quite: argv.quite,
             stopOnFirstFailure: argv.first
@@ -235,7 +285,6 @@ if (cmds.indexOf('serve') >= 0) {
     }
 } else {
     const configFile = getConfigFileSync();
-
     Promise.resolve()
         .then(() => {
             return configFile
