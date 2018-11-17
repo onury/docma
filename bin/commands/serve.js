5c5fd2cd19a13b41dfaebd47e027cbe5fcb02ebb
Onur Yıldırım <onur@cutepilot.com>
2018-11-17 23:54:21 +0300

    add --base option, revision

--- bin/commands/serve.js
+++ bin/commands/serve.js
@@ -19,43 +19,15 @@ const indexFiles = [];
 ['index', 'default', 'main', 'home', 'readme', 'guide']
     .forEach(name => indexFiles.push(`${name}.htm`, `${name}.html`));
 
-module.exports = (spaPath, options) => {
-
-    options = _.defaults(options, {
-        port: 9000,
-        quiet: false
-    });
-
+function serve(targetPath, options) {
     const url = `http://localhost:${options.port}`;
-    const pathIsSet = Boolean(spaPath);
-    let servePath = pathIsSet
-        ? path.resolve(spaPath)
-        : process.cwd();
-    let basePath = '/';
-
-    if (!fs.pathExistsSync(servePath)) {
-        console.error(chalk.red(`Cannot serve "${spaPath}". Path does not exist!`));
-        return;
-    }
-
-    const docmaConfigPath = utils._findDocmaConfigFileSync(servePath);
-    if (!pathIsSet && docmaConfigPath) {
-        console.info(chalk.gray(`» Found configuration file: ${docmaConfigPath}`));
-        console.info(chalk.gray(`  I will attempt to serve the configured destination.`));
-        console.info(chalk.gray(`  Provide a path (e.g. "./") to force serve target directory...\n`));
-        const conf = utils.json.readSync(docmaConfigPath);
-        if (conf && typeof conf.dest === 'string') {
-            servePath = path.resolve(process.cwd(), conf.dest);
-            if (conf.app && typeof conf.app.base === 'string') {
-                basePath = conf.app.base;
-            }
-        }
-    }
-
-    console.info(chalk.cyan(`Starting server @ path: ${servePath}`));
-
+    const basePath = options.base;
     const server = express();
-    server.use(basePath, serveStatic(servePath, { fallthrough: false, index: indexFiles }));
+    // see https://github.com/expressjs/serve-static
+    server.use(basePath, serveStatic(targetPath, {
+        fallthrough: false,
+        index: indexFiles
+    }));
     if (basePath !== '/') {
         server.get('/', (req, res) => res.redirect(basePath));
     }
@@ -74,9 +46,63 @@ module.exports = (spaPath, options) => {
             next();
         });
     }
+
+    console.info(chalk.cyan(`Starting server @ ${targetPath}`));
+    console.info(chalk.cyan(`App base path is: ${options.base}`));
     server.listen(options.port, () => {
         console.log('Serving SPA @', chalk.blue(`${url}${basePath}`));
-        console.log('');
+        console.log('...\n');
+    });
+}
+
+module.exports = (spaPath, options) => {
+    options = _.defaults(options, {
+        port: 9000,
+        base: null,
+        quiet: false
     });
 
+    const pathIsSet = typeof spaPath === 'string' && spaPath.length > 0;
+    const baseIsSet = typeof options.base === 'string' && options.base.length > 0;
+    let targetPath = pathIsSet
+        ? path.resolve(spaPath)
+        : process.cwd();
+    let basePath = baseIsSet
+        ? options.base
+        : '/';
+
+    function checkPath() {
+        if (!fs.pathExistsSync(targetPath)) {
+            console.error(chalk.red(`Cannot serve "${spaPath}". Path does not exist!`));
+            return false;
+        }
+        return true;
+    }
+
+    let conf, docmaConfigPath;
+    if (!pathIsSet || !baseIsSet) {
+        if (!checkPath()) return;
+        docmaConfigPath = utils._findDocmaConfigFileSync(targetPath);
+        conf = docmaConfigPath ? utils.json.readSync(docmaConfigPath) : null;
+    }
+
+    if (conf && !pathIsSet) {
+        console.info(chalk.gray(`» Found configuration file @ ${docmaConfigPath}`));
+        console.info(chalk.gray('  I will attempt to serve the configured destination.'));
+        console.info(chalk.gray('  Provide a path (e.g. "./") to force-serve target directory...\n'));
+        // const conf = utils.json.readSync(docmaConfigPath);
+        if (conf.dest && typeof conf.dest === 'string') {
+            targetPath = path.resolve(process.cwd(), conf.dest);
+        }
+    }
+    if (!checkPath()) return;
+
+    if (conf && !baseIsSet) {
+        if (conf.app && typeof conf.app.base === 'string') {
+            basePath = conf.app.base;
+        }
+    }
+
+    options.base = basePath;
+    serve(targetPath, options);
 };
